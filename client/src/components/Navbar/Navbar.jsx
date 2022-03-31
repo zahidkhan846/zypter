0b1890a34ff1dc945b074d745b77d011922548bc
Zahid Khan <zahid.khan846@hotmail.com>
2022-04-01 00:17:13 +0530

    date 1-4-2022 started adding navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,34 @@
+import { Link } from "react-router-dom";
 import Logo from "../../assets/logo.png";
 
 const Navbar = () => {
+  const navItems = [
+    { address: "/market", name: "Market" },
+    { address: "/exchange", name: "Exchange" },
+    { address: "/tutorials", name: "Tutorials" },
+    { address: "/wallets", name: "Wallets" },
+  ];
+
   return (
-    <header>
-      <nav>
-        <div>
-          <img src={Logo} alt="logo" />
-        </div>
+    <header className="navbar main-bg">
+      <img src={Logo} alt="logo" />
+      <nav className="navbar-nav">
+        <ul>
+          {navItems.map((item, index) => (
+            <NavItem key={index} item={item} />
+          ))}
+        </ul>
       </nav>
     </header>
   );
 };
 
 export default Navbar;
+
+const NavItem = ({ item }) => {
+  return (
+    <li>
+      <Link to={item.address}>{item.name}</Link>
+    </li>
+  );
+};
