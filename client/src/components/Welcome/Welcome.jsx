765a986325cc4ca69d5e14d0d5869ac0917b747c
Zahid Khan <zahid.khan846@hotmail.com>
2022-04-06 23:32:29 +0530

    complete welcome design

--- client/src/components/Welcome/Welcome.jsx
+++ client/src/components/Welcome/Welcome.jsx
@@ -1,9 +1,18 @@
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircleFill } from "react-icons/bs";
+import Input from "../UI/Input/Input";
+import { useState } from "react";
+import { Spinner } from "../UI/Spinner/Spinner";
+
 export const Welcome = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const handleConnectWallet = () => {};
 
+  const handleSubmit = () => {};
+
   return (
-    <div className="main-bg">
-      <div className="welcome-container">
+    <div className="welcome-container">
+      <div className="welcome-grid">
         <div className="welcome-text-container">
           <h1 className="text-bg">Send Crypto across the world</h1>
           <p>
@@ -23,6 +32,60 @@ export const Welcome = () => {
             <div className="single-feature">Web 3.0</div>
             <div className="single-feature">Low fees</div>
             <div className="single-feature">Blockchain</div>
+          </div>
+        </div>
+        <div className="card-container">
+          <div className="eth-card">
+            <div className="card-logo">
+              <div className="eth-logo">
+                <SiEthereum size={24} />
+              </div>
+              <BsInfoCircleFill size={18} />
+            </div>
+            <div className="card-text">
+              <p>0x29D7d1dd5B6f9C8...</p>
+              <p>Ethereum</p>
+            </div>
+          </div>
+          <div className="eth-form-container">
+            <Input
+              placeholder="ADDRESS TO"
+              type="text"
+              name="address-to"
+              value=""
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder="AMOUNT"
+              type="number"
+              name="amount"
+              value=""
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder="KEYWORD (GIF)"
+              type="text"
+              name="keyword"
+              value=""
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder="MESSAGE"
+              type="text"
+              name="message"
+              value=""
+              handleChange={() => {}}
+            />
+            <div className="underline"></div>
+            {isLoading ? (
+              <div className="form-spinner">
+                <Spinner />
+              </div>
+            ) : (
+              <div className="form-button">
+                <button onClick={handleSubmit}>SEND NOW</button>
+              </div>
+            )}
           </div>
         </div>
       </div>
